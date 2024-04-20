4d7395fb03f5dd9387c0c03d5137d36983f08708
FaidraA <f.anthopoulou28@gmail.com>
2024-04-20 22:49:09 +0300

    Update hotel.sql

--- hotel.sql
+++ hotel.sql
@@ -7,6 +7,8 @@
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
+-- Created by Faidra Anthopoulou
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
